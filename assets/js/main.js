d67b649482ef4f47319044f4ceec410f211d0863
ValeriyaS-code <85139016+ValeriyaS-code@users.noreply.github.com>
2021-07-24 16:06:04 -0400

    syntax error fixed fro local storage

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,12 +6,14 @@ $(".saveBtn").on("click", function() {
 });
 
 //getting data from local storage
-$("#9am.input").val(localStorage.getItem("9am"));
-$("#10am.input").val(localStorage.getItem("10am"));
-$("#11am.input").val(localStorage.getItem("11am"));
-$("#12pm.input").val(localStorage.getItem("12pm"));
-$("#1pm.input").val(localStorage.getItem("1pm"));
-$("#2pm.input").val(localStorage.getItem("2pm"));
-$("#3pm.input").val(localStorage.getItem("3pm"));
-$("#4pm.input").val(localStorage.getItem("4pm"));
-$("#5pm.input").val(localStorage.getItem("5pm"));
+$("#9am .input").val(localStorage.getItem("9am"));
+$("#10am .input").val(localStorage.getItem("10am"));
+$("#11am .input").val(localStorage.getItem("11am"));
+$("#12pm .input").val(localStorage.getItem("12pm"));
+$("#1pm .input").val(localStorage.getItem("1pm"));
+$("#2pm .input").val(localStorage.getItem("2pm"));
+$("#3pm .input").val(localStorage.getItem("3pm"));
+$("#4pm .input").val(localStorage.getItem("4pm"));
+$("#5pm .input").val(localStorage.getItem("5pm"));
+
+
